30c04a9fa454bad1474e24099f0ad80871d98a43
ZhangQiang <zhangqiangoffice@163.com>
2016-03-11 18:41:24 +0800

    新增天气查询部分功能

---
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -0,0 +1,8 @@
+package com.coolweather.app.activity;
+
+import android.app.Activity;
+import android.view.View.OnClickListener;
+
+public class WeatherActivity extends Activity implements OnClickListener{
+	
+}
